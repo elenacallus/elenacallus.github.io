a6b65532f9f58117c6591877ca211d016e5a206d
elenacallus <elenacallus@gmail.com>
2025-10-03 15:22:27 +0000

    Deploying to gh-pages from @ elenacallus/elenacallus.github.io@865c6a53bb807790bf461d789f4017f88db5308d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "publications",
-          description: "all publications can be found on my [arXiv page](https://arxiv.org/a/callus_e_1.html).",
+          description: "all publications can be found on my arXiv page.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
