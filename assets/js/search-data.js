d0e374cedc3159c1e872efad6228cd2cc1e3d52b
elenacallus <elenacallus@gmail.com>
2025-10-04 18:28:10 +0000

    Deploying to gh-pages from @ elenacallus/elenacallus.github.io@815b3adc0a65b9e5158db99069aaf12a5344cce5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "below is an overview of my CV. a complete CV can be accessed through the PDF icon on the right.",
+          description: "my complete CV can be accessed through the PDF icon on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
